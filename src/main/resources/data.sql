6b6184f80ab751d562123ffe88a6252ae836e683
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2019-09-07 15:08:50 +0200

    data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
 insert into CAR (id, brand, model, mileage, isDamaged, isAvailable, pricePerDay)
-values ('PP42069', 'Fiat', 'Multipla', '123456', '0', '1', '150');
+values ('PP42069', 'Fiat', 'Multipla', '123456', '0', '1', '150'),
+('PO00123', 'Ford', 'Kuga', '20100', '0', '1', '300'),
+('PO5553', 'Mazda', '3', '5000', '0', '1', '250'),
+('PO9933', 'VW', 'Passat', '12000', '0', '1' , '300'),
+('PO4343', 'BMW', '5', '8000', '0', '1', '550');
 
 commit;
