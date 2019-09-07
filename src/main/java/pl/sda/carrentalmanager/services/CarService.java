ee376c7a588b7b084c5483d05747353f25f3c950
Michu <mchbzowy@gmail.com>
2019-09-07 15:21:12 +0200

    Bez SQLa

--- src/main/java/pl/sda/carrentalmanager/services/CarService.java
+++ src/main/java/pl/sda/carrentalmanager/services/CarService.java
@@ -1,4 +1,10 @@
 package pl.sda.carrentalmanager.services;
 
 public class CarService {
+
+    private  final CarService carService;
+
+    public CarService(CarService carService) {
+        this.carService = carService;
+    }
 }
---
+++ src/main/java/pl/sda/carrentalmanager/services/ClientService.java
@@ -0,0 +1,25 @@
+package pl.sda.carrentalmanager.services;
+
+import org.springframework.stereotype.Service;
+import pl.sda.carrentalmanager.models.Client;
+import pl.sda.carrentalmanager.repositories.ClientRepository;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+
+    }
+
+
+    public List<Client> getAll() {
+        return clientRepository.findAll();
+
+    }
+
+}
