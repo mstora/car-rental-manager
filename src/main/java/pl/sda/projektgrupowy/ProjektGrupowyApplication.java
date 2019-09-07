755bac482ae87f875039ea5942c8a997d56ae1a9
marek <marek.stora@stora.com>
2019-09-07 09:51:28 +0200

    Initial commit

---
+++ src/main/java/pl/sda/projektgrupowy/ProjektGrupowyApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.projektgrupowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGrupowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektGrupowyApplication.class, args);
+    }
+
+}
