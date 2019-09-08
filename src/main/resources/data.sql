6e845c6c39ed7e879e4189f14ad5c83e49d6abb7
marek <marek.stora@stora.com>
2019-09-08 11:42:25 +0200

    added initial data inserts for cars, clients, rentals #12 #13 #14

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,26 @@ insert into CAR (ID, BRAND, MODEL, MILEAGE, IS_DAMAGED, IS_AVAILABLE, PRICE_PER_
 values ('PP42069', 'Fiat', 'Multipla', 123456, 0, 1, 150),
 ('PO00123', 'Ford', 'Kuga', 20100, 0,1, 300),
 ('PO5553', 'Mazda', '3', 5000, 0, 1, 250),
-('PO9933', 'VW', 'Passat', 12000, 0, 1 , 300),
-('PO4343', 'BMW', '5', 8000, 0, 1, 550);
+('PO9933', 'VW', 'Passat', 12000, 0, 0 , 300),
+('PO4343', 'BMW', '5', 8000, 0, 0, 550);
+
+insert into CLIENT (id, name, last_name, date_of_birth)
+values ('90101012345', 'Lukasz', 'Tararynowicz', '1990-10-10'),
+('87122512345', 'Kazmierz', 'Nowak', '1987-12-25'),
+('92050812345', 'Koteł', 'Pieseł', '1992-05-08'),
+('69042012345', 'Krzysztof', 'Kups', '1969-04-20'),
+('94070712345', 'Krystyna', 'Czubowna', '1994-07-07'),
+('90022812345', 'Jeżuś', 'Chytrus', '1990-02-28');
+
+insert into RENTAL (id, car_id, client_id, date_of_rental,
+                    date_of_return, mileage, is_damaged,
+                    is_rented, deposit, total_price)
+values
+(nextval('rent_seq'), 'PP42069', '87122512345', '2019-01-20', '2019-01-22', 120, 0, 0, 100, 300),
+(nextval('rent_seq'),'PP42069', '69042012345', '2019-02-05', '2019-02-10', 300, 0, 0, 250, 750),
+(nextval('rent_seq'),'PO00123', '90101012345', '2019-03-17', '2019-03-20', 500, 0, 0, 300, 900),
+(nextval('rent_seq'),'PO5553', '92050812345', '2019-08-20', '2019-08-27', 700, 0, 0, 400, 1750),
+(nextval('rent_seq'),'PO9933', '94070712345', '2019-09-07', '2019-09-10', 0, 0, 1, 300, 900),
+(nextval('rent_seq'),'PO4343', '90022812345', '2019-09-06', '2019-09-12', 0, 0, 1, 1000, 3300);
 
 commit;
